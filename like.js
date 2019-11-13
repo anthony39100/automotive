14e832019f8f0971176c71ed07d058c167a0410c
anthony39100 <anthonybreviglieri.ab@gmail.com>
2019-11-13 10:13:09 +0000

    recup fichier quentin

---
+++ like.js
@@ -0,0 +1,24 @@
+var like=document.querySelector(".button-like")
+var compteur=99999
+var bool=true
+var chiffre=document.querySelector(".chiffre-like")
+like.addEventListener("click",()=>{
+    if(bool===false){
+    var chiffre=document.querySelector(".chiffre-like")
+    chiffre.innerText=compteur
+    compteur--
+    bool=true
+    }
+    else{
+        var chiffre=document.querySelector(".chiffre-like")
+        chiffre.innerText=compteur
+       
+        compteur++
+
+        bool=false
+    }
+})
+window.addEventListener("load",()=>{
+    chiffre.innerText=compteur
+
+})
